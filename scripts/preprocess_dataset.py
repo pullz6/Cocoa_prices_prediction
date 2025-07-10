c5ef08fd7d9ff365ebd3cf967cd7f2516de49f6f
Sara <gunawardhanapulsara@gmail.com>
2025-07-10 21:18:12 +0100

    fix:functiont

--- scripts/preprocess_dataset.py
+++ scripts/preprocess_dataset.py
@@ -5,16 +5,22 @@ from pathlib import Path
 data_path = Path(__file__).parent.parent / "data" / "Daily_Prices_Home_NEW.csv"
 
 df = pd.read_csv(data_path)
-df['Date'] = pd.to_datetime(df['Date'],format='%d/%m/%Y')
 
-df['London futures (£ sterling/tonne)'] = df['London futures (£ sterling/tonne)'].str.replace(',', '')
-df['London futures (£ sterling/tonne)'] = df['London futures (£ sterling/tonne)'].astype(float)
+def preprocess_data(df): 
+    '''General preprocessing, converting columns to the requires data types'''
+    df['Date'] = pd.to_datetime(df['Date'],format='%d/%m/%Y')
 
-df['New York futures (US$/tonne)'] = df['New York futures (US$/tonne)'].str.replace(',', '')
-df['New York futures (US$/tonne)'] = df['New York futures (US$/tonne)'].astype(float)
+    df['London futures (£ sterling/tonne)'] = df['London futures (£ sterling/tonne)'].str.replace(',', '')
+    df['London futures (£ sterling/tonne)'] = df['London futures (£ sterling/tonne)'].astype(float)
 
-df['ICCO daily price (US$/tonne)'] = df['ICCO daily price (US$/tonne)'].str.replace(',', '')
-df['ICCO daily price (US$/tonne)'] = df['ICCO daily price (US$/tonne)'].astype(float)
+    df['New York futures (US$/tonne)'] = df['New York futures (US$/tonne)'].str.replace(',', '')
+    df['New York futures (US$/tonne)'] = df['New York futures (US$/tonne)'].astype(float)
 
-df['ICCO daily price (Euro/tonne)'] = df['ICCO daily price (Euro/tonne)'].str.replace(',', '')
-df['ICCO daily price (Euro/tonne)'] = df['ICCO daily price (Euro/tonne)'].astype(float)
+    df['ICCO daily price (US$/tonne)'] = df['ICCO daily price (US$/tonne)'].str.replace(',', '')
+    df['ICCO daily price (US$/tonne)'] = df['ICCO daily price (US$/tonne)'].astype(float)
+
+    df['ICCO daily price (Euro/tonne)'] = df['ICCO daily price (Euro/tonne)'].str.replace(',', '')
+    df['ICCO daily price (Euro/tonne)'] = df['ICCO daily price (Euro/tonne)'].astype(float)
+    
+    return df
+
