0d206dec7debbc21c215782c10c038579c99da13
Sara <gunawardhanapulsara@gmail.com>
2025-07-12 11:46:49 +0100

    fix: date read ISO8601

--- scripts/utils_and_constants.py
+++ scripts/utils_and_constants.py
@@ -7,4 +7,5 @@ def read_data(name):
     '''Reading the data'''
     data_path = Path(__file__).parent.parent / "data" / name
     df = pd.read_csv(data_path)
+    df['Date'] = pd.to_datetime(df['Date'],format='ISO8601')
     return df
