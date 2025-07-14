35a75d18161012377015908e61c9686a60cdf571
Sara <gunawardhanapulsara@gmail.com>
2025-07-14 23:56:54 +0100

    fix:added mlflow in

--- scripts/model.py
+++ scripts/model.py
@@ -1,8 +1,14 @@
 import pandas as pd 
 import numpy as np 
 
+import mlflow
+
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
+
+mlflow.set_tracking_uri("http://localhost:5000")
+
+mlflow.start_run()
 
 
 
